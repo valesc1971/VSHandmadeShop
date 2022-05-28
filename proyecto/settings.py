0c1f1414f414fea434d10f716d87a652ab6eeea3
valeria_sanchez_c@hotmail.com <valeria_sanchez_c@hotmail.com>
2022-05-28 17:07:44 -0400

    deployment set up

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -28,9 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fdi-@d5aj25i^lzaeezx%d5sbvm0y3+av=0-x(eh*e%zj%!y7h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['vshandmadeshop.herokuapp.com']  
 
-ALLOWED_HOSTS = []  #['localhost', '127.0.0.1']
+
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -52,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +140,13 @@ THOUSAND_SEPARATOR = "."
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+ )   
+
 #STATICFILES_DIRS=[os.path.join(BASE_DIR,"static"), "templates"]
 
 # Default primary key field type
