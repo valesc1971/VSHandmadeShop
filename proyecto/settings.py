66830c8726de2c3bca55953d3706fd1a4cf9e599
valeria_sanchez_c@hotmail.com <valeria_sanchez_c@hotmail.com>
2022-05-31 10:03:10 -0400

    send email after checkout

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fdi-@d5aj25i^lzaeezx%d5sbvm0y3+av=0-x(eh*e%zj%!y7h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
 ALLOWED_HOSTS = ['vshandmadeshop.herokuapp.com', '127.0.0.1']  
 
 
